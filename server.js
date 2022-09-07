87d625853300f5be5e468d97a98c68078bd5351a
Haley Seymour <haleycseymour@comcast.net>
2022-09-06 21:59:09 -0700

    allow cors

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const sequelize = require('./config/connection');
 const controllers = require('./controllers');
+const cors = require("cors")
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
 
 app.use(controllers);
 
